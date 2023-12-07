d4fe2142f526f016a28a95002808121d800916f4
Yusuf Kaymaz <kaymazyusuf88@gmail.com>
2023-12-07 20:28:35 +0300

    js data file

--- app.js
+++ app.js
@@ -1,80 +1,196 @@
-const el = document.querySelector(".blaze-slider");
-const config = {
-  // items: 5,
-  all: {
-    loop: false,
-    slidesToShow: 4,
-    transitionTimingFunction: "linear",
-  },
-  "(max-width: 900px)": {
-    slidesToShow: 2,
-  },
-  "(max-width: 500px)": {
-    slidesToShow: 1,
-  },
-};
-new BlazeSlider(el, config);
+document.addEventListener("DOMContentLoaded", function () {
+  const el = document.querySelector(".blaze-slider");
+  const config = {
+    all: {
+      slidesToShow: 4,
+      transitionTimingFunction: "linear",
+    },
+    "(max-width: 900px)": {
+      slidesToShow: 2,
+    },
+    "(max-width: 500px)": {
+      slidesToShow: 1,
+    },
+  };
+  const cardDataArray = [
+    {
+      brandName: "Marka Adı1",
+      description: "Ürün açıklaması 1",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "Marka Adı2",
+      description: "Ürün açıklaması 2",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "Marka Adı3",
+      description: "Ürün açıklaması 3",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "Marka Adı4",
+      description: "Ürün açıklaması 1",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "Marka Adı5",
+      description: "Ürün açıklaması 2",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "Marka Adı6",
+      description: "Ürün açıklaması 3",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "Marka Adı7",
+      description: "Ürün açıklaması 1",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "Marka Adı8",
+      description: "Ürün açıklaması 2",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "Marka Adı9",
+      description: "Ürün açıklaması 3",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    
+  ];
+  const cardDataArray2 = [
+    {
+      brandName: "FarklıMarka Adı1",
+      description: "Ürün açıklaması 1",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "FarklıMarka Adı2",
+      description: "Ürün açıklaması 2",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "FarklıMarka Adı3",
+      description: "Ürün açıklaması 3",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "FarklıMarka Adı4",
+      description: "Ürün açıklaması 1",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "FarklıMarka Adı5",
+      description: "Ürün açıklaması 2",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "FarklıMarka Adı6",
+      description: "Ürün açıklaması 3",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "FarklıMarka Adı7",
+      description: "Ürün açıklaması 1",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "FarklıMarka Adı8",
+      description: "Ürün açıklaması 2",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    {
+      brandName: "FarklıMarka Adı9",
+      description: "Ürün açıklaması 3",
+      discountLink: "#",
+      oldPrice: "250,90 TL",
+      newPrice: "215,90 TL",
+      imgUrl: "assets/images/havuc.png",
+    },
+    
+  ];
 
-const newests = [
-  {
-    brandName: "Marka Adı1",
-    description:
-      "Ürün adı bu alanda yer alıyor ve iki satır olarak uzunlukta gelebilir",
-    discount: "%20 İNDİRİM",
-    oldPrice: "250,90 TL",
-    newPrice: "215,90 TL",
-    imgUrl: "assets/images/havuc.png",
-  },
-];
-
-const cardDataArray = [
-  {
-    brandName: "Marka Adı1",
-    description: "Ürün açıklaması 1",
-    discountLink: "#",
-    oldPrice: "250,90 TL",
-    newPrice: "215,90 TL",
-  },
-  {
-    brandName: "Marka Adı2",
-    description: "Ürün açıklaması 2",
-    discountLink: "#",
-    oldPrice: "250,90 TL",
-    newPrice: "215,90 TL",
-  },
-  {
-    brandName: "Marka Adı3",
-    description: "Ürün açıklaması 3",
-    discountLink: "#",
-    oldPrice: "250,90 TL",
-    newPrice: "215,90 TL",
-  },
-  // Diğer kart verilerini ekleyebilirsiniz
-];
-
-// function createCardElement(cardData) {
-//   const cardElement = document.createElement("div");
-//   cardElement.classList.add(
-//     "card",
-//     "flex-column",
-//     "justify-content-center",
-//     "align-items-center"
-//   );
-//   cardElement.innerHTML = `
-//     <img src="assets/images/havuc.png" alt="havuc" />
-//     <p class="brand-name">${cardData.brandName}</p>
-//     <p class="description">${cardData.description}</p>
-//     <a class="discount" href="${cardData.discountLink}">%20 İNDİRİM</a>
-//     <span class="old-price">${cardData.oldPrice}</span>
-//     <span class="new-price">${cardData.newPrice}</span>
-//   `;
-//   return cardElement;
-// }
+  function createCardElement(cardData) {
+    // örnek kart
+    const cardElement = document.createElement("div");
+    cardElement.classList.add(
+      "card",
+      "flex-column",
+      "justify-content-center",
+      "align-items-center"
+    );
+    cardElement.innerHTML = `
+      <img src="${cardData.imgUrl}" alt="havuc" />
+      <p class="brand-name">${cardData.brandName}</p>
+      <p class="description">${cardData.description}</p>
+      <a class="discount" href="${cardData.discountLink}">%20 İNDİRİM</a>
+      <span class="old-price">${cardData.oldPrice}</span>
+      <span class="new-price">${cardData.newPrice}</span>
+    `;
+    return cardElement;
+  }
 
 
-// // Kartları HTML'e ekleme
-// const cardContainer = document.querySelector(".blaze-track");
-// cardDataArray.forEach((cardData) => {
-//   const cardElement = createCardElement(cardData);
-//   cardContainer.appendChild(cardElement);
-// });
+  // Kartları html e ekleme
+
+  const cardContainer = document.querySelector(".blaze-track");
+  cardDataArray2.forEach((cardData) => {
+    const cardElement = createCardElement(cardData);
+    cardContainer.appendChild(cardElement);
+  });
+
+  new BlazeSlider(el, config);
+});
