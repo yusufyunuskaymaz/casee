3093d28f9b398e1330f793da452eaaf9aa34e346
Yusuf Kaymaz <kaymazyusuf88@gmail.com>
2023-12-08 03:23:49 +0300

    bookmark addition

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
       oldPrice: "250,90 TL",
       newPrice: "215,90 TL",
       imgUrl1: "assets/images/pizza.png",
-      imgUrl2: "assets/images/pizza.png",
+      imgUrl2: "assets/images/img3.png",
       bookMark:true
 
     },
@@ -87,16 +87,15 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       brandName: "Et Biftek",
       description: "Ürün açıklaması 1",
-
       oldPrice: "250,90 TL",
       newPrice: "215,90 TL",
       imgUrl1: "assets/images/et.png",
       imgUrl2: "assets/images/img1.png",
+      bookMark:true
     },
     {
       brandName: "Yoğurtlu Makarna",
       description: "Ürün açıklaması 2",
-
       oldPrice: "250,90 TL",
       newPrice: "215,90 TL",
       imgUrl1: "assets/images/makarna.png",
@@ -105,16 +104,15 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       brandName: "Havuçlu Makarna",
       description: "Ürün açıklaması 3",
-
       oldPrice: "250,90 TL",
       newPrice: "215,90 TL",
       imgUrl1: "assets/images/havuc.png",
       imgUrl2: "assets/images/img3.png",
+      bookMark:true
     },
     {
       brandName: "Pizza",
       description: "Ürün açıklaması 1",
-
       oldPrice: "250,90 TL",
       newPrice: "215,90 TL",
       imgUrl1: "assets/images/pizza.png",
@@ -123,7 +121,6 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       brandName: "FarklıMarka Adı5",
       description: "Ürün açıklaması 2",
-
       oldPrice: "250,90 TL",
       newPrice: "215,90 TL",
       imgUrl1: "assets/images/et.png",
@@ -132,7 +129,6 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       brandName: "FarklıMarka Adı6",
       description: "Ürün açıklaması 3",
-
       oldPrice: "250,90 TL",
       newPrice: "215,90 TL",
       imgUrl1: "assets/images/makarna.png",
@@ -141,16 +137,15 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       brandName: "FarklıMarka Adı7",
       description: "Ürün açıklaması 1",
-
       oldPrice: "250,90 TL",
       newPrice: "215,90 TL",
       imgUrl1: "assets/images/havuc.png",
       imgUrl2: "assets/images/img3.png",
+      bookMark:true
     },
     {
       brandName: "FarklıMarka Adı8",
       description: "Ürün açıklaması 2",
-
       oldPrice: "250,90 TL",
       newPrice: "215,90 TL",
       imgUrl1: "assets/images/pizza.png",
@@ -159,7 +154,6 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       brandName: "FarklıMarka Adı9",
       description: "Ürün açıklaması 3",
-
       oldPrice: "250,90 TL",
       newPrice: "215,90 TL",
       imgUrl1: "assets/images/makarna.png",
@@ -323,7 +317,7 @@ document.addEventListener("DOMContentLoaded", function () {
     all: {
       slidesToShow: 4,
       transitionTimingFunction: "linear",
-      loop: true,
+      loop: false,
     },
     "(max-width: 900px)": {
       slidesToShow: 2,
